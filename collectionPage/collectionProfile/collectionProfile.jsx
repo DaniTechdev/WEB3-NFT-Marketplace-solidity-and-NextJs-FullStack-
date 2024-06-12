132e584273442103aab1c5d40323e4a3ddb993b0
DaniTech <tochidan11@gmail.com>
2024-06-12 07:02:00 -0700

    Done with Building and Styling the collection Profile component

--- collectionPage/collectionProfile/collectionProfile.jsx
+++ collectionPage/collectionProfile/collectionProfile.jsx
@@ -1,11 +1,71 @@
 import React from "react";
 import Image from "next/image";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialInstagram,
+} from "react-icons/ti";
 
 //INTERNAL IMPORT
-import Style from "../";
+import Style from "./collectionProfile.module.css";
+import images from "../../img";
 
 const collectionProfile = () => {
-  return <div>collectionProfile</div>;
+  const cardArray = [1, 2, 3, 4];
+
+  return (
+    <div className={Style.collectionProfile}>
+      <div className={Style.collectionProfile_box}>
+        <div className={Style.collectionProfile_box_left}>
+          <Image
+            src={images.nft_image_1}
+            alt="nft image"
+            width={800}
+            height={800}
+            className={Style.collectionProfile_box_left_img}
+          />
+
+          <div className={Style.collectionProfile_box_left_social}>
+            <a href="#">
+              <TiSocialFacebook />
+            </a>
+            <a href="#">
+              <TiSocialInstagram />
+            </a>
+            <a href="#">
+              <TiSocialLinkedin />
+            </a>
+            <a href="#">
+              <TiSocialTwitter />
+            </a>
+          </div>
+        </div>
+
+        <div className={Style.collectionProfile_box_middle}>
+          <h1>Awesome NFTs collection</h1>
+          <p>
+            Karafuru is home of 5,555 generaative arts where colors reign
+            supreme. Leave the drab reality and enter the world of KArafuru
+            Musuem toy.
+          </p>
+
+          <div className={Style.collectionProfile_box_middle_box}>
+            {cardArray.map((el, i) => (
+              <div
+                className={Style.collectionProfile_box_middle_box_item}
+                key={i + 1}
+              >
+                <small>Floor price</small>
+                <p>${i + 1} 95,4687</p>
+                <span>+{i + 2}.11% </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default collectionProfile;
