732180c6b7ac64a6ad02a80141a3bbed77ad2b2a
DaniTech <tochidan11@gmail.com>
2024-06-17 13:16:15 -0700

    Done with NFT image component styling with css

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -4,11 +4,14 @@ import React from "react";
 
 import Style from "./Button.module.css";
 
-const Button = ({ btnText, handleClick }) => {
+const Button = ({ btnText, handleClick, icon, classStyle }) => {
   return (
     <div className={Style.box}>
-      <button className={Style.button} onClick={() => handleClick()}>
-        {btnText}
+      <button
+        className={`${Style.button} ${classStyle}`}
+        onClick={() => handleClick()}
+      >
+        {icon} {btnText}
       </button>
     </div>
   );
