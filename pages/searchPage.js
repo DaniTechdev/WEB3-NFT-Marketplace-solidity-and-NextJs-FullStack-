ef0bcfd514b2347b40c65ca245a523c5e9500797
DaniTech <tochidan11@gmail.com>
2024-06-17 04:28:11 -0700

    Started searchBar Page by crreating neccesary components

---
+++ pages/searchPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+//INTERNAL IMPORT
+import Style from "../styles/searchPage.module.css";
+
+const searchPage = () => {
+  return <div>searchPage</div>;
+};
+
+export default searchPage;
